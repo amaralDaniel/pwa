355a3b0aff3e8cdae7d12ce4b16420bb3eb4b87e
Daniel Amaral <amaral94daniel@gmail.com>
2018-04-19 02:13:59 +0100

    404 page done

--- github-client/src/components/NotFound.vue
+++ github-client/src/components/NotFound.vue
@@ -1,8 +1,18 @@
 <template>
-    <div>
-        {{ name }}
-    </div>
-
+  <v-container grid-list-md class="jumbotron-container">
+    <v-jumbotron color="grey lighten-2">
+      <v-container fill-height>
+        <v-layout align-center text-sm-left>
+          <v-flex>
+            <h3 class="display-3">404</h3>
+            <span class="subheading">Page not found.</span>
+            <v-divider class="my-3"></v-divider>
+            <router-link :to="{name: 'repos'}"><v-btn large color="primary" class="mx-0">Get back to safety</v-btn></router-link>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-jumbotron>
+  </v-container>
 </template>
 
 <script>
@@ -16,6 +26,7 @@
   }
 </script>
 
-<style scoped>
-
+<style lang="sass" scoped>
+  .jumbotron-container
+    max-width: 100%
 </style>
